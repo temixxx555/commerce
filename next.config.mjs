21e7934466dca7312e729cd69b226cb3ba30ef1c
Adebayo Temi <temiq3@gmail.com>
2025-05-01 20:35:32 +0100

    based on beliefs

--- next.config.mjs
+++ next.config.mjs
@@ -1,30 +1,34 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    images: {
-      remotePatterns: [
-        {
-          protocol: 'https',
-          hostname: 'res.cloudinary.com',
-          pathname: '**',
-        },
-        {
-          protocol: 'https',
-          hostname: 'raw.githubusercontent.com',
-          pathname: '**',
-        },
-        {
-          protocol: 'https',
-          hostname: 'imgur.com',
-          pathname: '**',
-        },
-        {
-          protocol: 'https',
-          hostname: 'static.wixstatic.com',
-          pathname: '**',
-        },
-      ],
-    },
-  };
-  
-  export default nextConfig;
-  
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'res.cloudinary.com',
+        pathname: '**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'raw.githubusercontent.com',
+        pathname: '**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'imgur.com',
+        pathname: '**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'static.wixstatic.com',
+        pathname: '**',
+      },
+    ],
+  },
+  experimental: {
+    serverActions: true, // optional if you're using server actions
+  },
+  output: 'standalone', // allows for dynamic rendering when deployed to Vercel
+  reactStrictMode: true,
+};
+
+export default nextConfig;
