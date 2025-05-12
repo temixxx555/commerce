577a2396b0adc8574a5b6e2f11c3b4d91ef54b63
Adebayo Temi <temiq3@gmail.com>
2025-05-12 21:09:21 +0100

    updated code

--- models/Address.js
+++ models/Address.js
@@ -4,7 +4,7 @@ const addressSchema = new mongoose.Schema({
   userId: { type: String, required: true },
   fullName: { type: String, required: true },
   phoneNumber: { type: String, required: true },
-  pincode: { type: Number, required: true },
+  pincode: { type: String, required: true },
   area:{type:String,required:true},
   city:{type:String,required:true},
   state:{type:String,required:true}
